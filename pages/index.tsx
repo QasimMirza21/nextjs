0dc092162b938dbcac0df1730b699be7f21f6508
QasimMirza21 <7000themaster@gmail.com>
2025-06-08 15:54:30 +0200

    Update index.tsx2

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,66 +7,62 @@ type Props = {
   pets: Pets[];
 };
 
-const Index = ({ pets }: Props) => {
+const IndexPage = ({ pets }: Props) => {
   return (
     <>
-      {pets.map((pet) => (
-        <div key={pet._id as string}>
-          <div className="card">
-            <img src={pet.image_url} />
-            <h5 className="pet-name">{pet.name}</h5>
-            <div className="main-content">
-              <p className="pet-name">{pet.name}</p>
-              <p className="owner">Owner: {pet.owner_name}</p>
+      {pets.map((pet) => {
+        const id = pet._id as string;
 
-              {/* Extra Pet Info: Likes and Dislikes */}
-              <div className="likes info">
-                <p className="label">Likes</p>
-                <ul>
-                  {pet.likes.map((data, index) => (
-                    <li key={index}>{data} </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="dislikes info">
-                <p className="label">Dislikes</p>
-                <ul>
-                  {pet.dislikes.map((data, index) => (
-                    <li key={index}>{data} </li>
-                  ))}
-                </ul>
-              </div>
+        return (
+          <div key={id}>
+            <div className="card">
+              <img src={pet.image_url} />
+              <h5 className="pet-name">{pet.name}</h5>
+              <div className="main-content">
+                <p className="pet-name">{pet.name}</p>
+                <p className="owner">Owner: {pet.owner_name}</p>
 
-              <div className="btn-container">
-                <Link href={{ pathname: "/[id]/edit", query: { id: pet._id } }}>
-                  <button className="btn edit">Edit</button>
-                </Link>
-                <Link href={{ pathname: "/[id]", query: { id: pet._id } }}>
-                  <button className="btn view">View</button>
-                </Link>
+                <div className="likes info">
+                  <p className="label">Likes</p>
+                  <ul>
+                    {pet.likes?.map((data, index) => (
+                      <li key={index}>{data}</li>
+                    ))}
+                  </ul>
+                </div>
+
+                <div className="dislikes info">
+                  <p className="label">Dislikes</p>
+                  <ul>
+                    {pet.dislikes?.map((data, index) => (
+                      <li key={index}>{data}</li>
+                    ))}
+                  </ul>
+                </div>
+
+                <div className="btn-container">
+                  <Link href={{ pathname: "/[id]/edit", query: { id } }}>
+                    <button className="btn edit">Edit</button>
+                  </Link>
+                  <Link href={{ pathname: "/[id]", query: { id } }}>
+                    <button className="btn view">View</button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 };
 
-/* Retrieves pet(s) data from mongodb database */
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   await dbConnect();
+  const result = await Pet.find({}).lean();
+  const pets = JSON.parse(JSON.stringify(result));
 
-  /* find all the data in our database */
-  const result = await Pet.find({});
-
-  /* Ensures all objectIds and nested objectIds are serialized as JSON data */
-  const pets = result.map((doc) => {
-    const pet = JSON.parse(JSON.stringify(doc));
-    return pet;
-  });
-
-  return { props: { pets: pets } };
+  return { props: { pets } };
 };
 
-export default Index;
+export default IndexPage;
